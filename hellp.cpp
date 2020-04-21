1a82f3e8d5053178411c5a9aee266868058c2a56
LiFu-ChrisLee <753641657@qq.com>
2020-04-21 12:53:08 +0000

    hello

---
+++ hellp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+  int age;
+  cin >> age;
+  cout << "Hello world! I'm " << age << "!" << endl;
+  return 0;
+}
